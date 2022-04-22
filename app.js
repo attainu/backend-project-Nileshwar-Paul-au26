e8787e81074eed76fabb075c3faf6e150b905a03
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-04-22 21:30:53 +0530

    express static err removed

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.engine('.hbs',exphbs.engine({defaultLayout:'main',extname:'.hbs'}));
 app.set('view engine','.hbs');
 
 
-app.use('*',express.static(path.join(__dirname,'public')))
+app.use(express.static('public'))
 
 // Body parser
 app.use(express.urlencoded({ extended: false }))
