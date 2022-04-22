495bf80fca8cc6233f942e8a40d15b9d325e195d
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-04-22 17:17:20 +0530

    set template engine

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 dotenv.config()
 const {connectDB} = require('./config/db.js');
 
@@ -12,5 +12,11 @@ const app = express();
 //connection to database
 connectDB()
 
+//loging
+app.use(morgan('dev'))
+
+//seting the template enging
+app.engine('.hbs',exphbs({defaultLayout:'main',extname:'.hbs'}));
+app.set('view engine','.hbs');
 
 app.listen(process.env.PORT,console.log(`Server Started at Port No ${process.env.PORT}`))
