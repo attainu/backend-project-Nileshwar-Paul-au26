d4f62ccb515c20104630820a28e760a77b837d1a
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-04-22 16:08:13 +0530

    server started

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+dotenv.config()
+
+const app = express();
+
+app.listen(process.env.PORT,console.log(`Server Started at Port No ${process.env.PORT}`))
