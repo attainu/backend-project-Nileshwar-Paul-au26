aeb13ad44d403264b2e9b057bbefffbc8dea269a
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-04-22 19:55:40 +0530

    express.static() not working

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const path = require('path')
 dotenv.config()
 const {connectDB} = require('./config/db.js');
+const {router}=require('./routes/index.js');
 
 //initiationg the express
 const app = express();
@@ -12,11 +14,22 @@ const app = express();
 //connection to database
 connectDB()
 
-//loging
-app.use(morgan('dev'))
+//loging*
+//app.use(morgan('dev'))
 
-//seting the template enging
-app.engine('.hbs',exphbs({defaultLayout:'main',extname:'.hbs'}));
+//seting the template engine *
+app.engine('.hbs',exphbs.engine({defaultLayout:'main',extname:'.hbs'})); 
 app.set('view engine','.hbs');
 
+
+app.use('*',express.static(path.join(__dirname,'public')))
+
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+//starting the server
 app.listen(process.env.PORT,console.log(`Server Started at Port No ${process.env.PORT}`))
+
+//routing
+app.use('/',router)
