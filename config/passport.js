c1db06cba4c6df30794ff279cd6b0ff08c8a7375
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-04-28 19:09:51 +0530

    update

--- config/passport.js
+++ config/passport.js
@@ -16,39 +16,39 @@ module.exports = function (passport) {
             // done = callback()
             // Here we are constructing the new user !
             const newUser = {
-                googleId :profile.id,
-                displayName : profile.displayName,
-                firstName :profile.name.givenName,
-                lastName :profile.name.familyName,
-                image:profile.photos[0].value
+                googleId: profile.id,
+                displayName: profile.displayName,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                image: profile.photos[0].value
             }
 
             // Checking if the user exist in our database or not !
             try {
-                let user = await User.findOne({googleId:profile.id})
-                if(user){
-                    done(null,user)
+                let user = await User.findOne({ googleId: profile.id })
+                if (user) {
+                    done(null, user)
                 }
-                else{
+                else {
                     user = await User.create(newUser)
-                    done(null,user)
+                    done(null, user)
                 }
             } catch (err) {
-                console.error(err) 
-                
+                console.error(err)
+
             }
         }))
 
-        
+
     // Each subsequent request  will not contain credentials , but rather than unique cookie that identifies the session. In order  to support login sessions, Passport will serialize and deserialize 'user' instances to and from the session.
-    passport.serializeUser((user,done) =>{
+    passport.serializeUser((user, done) => {
         // The user.id will be saved to the session (req.session.passport.user = {id : '..'}) !
-        done(null , user.id)
+        done(null, user.id)
     })
-    
-    passport.deserializeUser((id,done)=>{
+
+    passport.deserializeUser((id, done) => {
         // user object attaches to the request as req.user !
-        User.findById(id,(err,user)=>done(err,user))
+        User.findById(id, (err, user) => done(err, user))
     })
 }
 
