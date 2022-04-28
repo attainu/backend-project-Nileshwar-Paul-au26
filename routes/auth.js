6551804b0802682dde4bacec155901d336aea3c3
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-04-28 19:10:28 +0530

    update

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,9 @@ const express = require('express');
 const passport = require('passport');
 const authrouter = express.Router();
 
-
 // @description:-   Auth with Google 
 // @route:-         GET/auth/google
 authrouter.get('/google', passport.authenticate('google', { scope: ['profile'] }))
-
 
 // @description:-   Google Auth callback
 // @route:-         GET/auth/google/callback
@@ -18,10 +16,9 @@ authrouter.get(
     }
 )
 
-
 // @description:- Logout User
 // route:-        /auth/logout
-authrouter.get('/logout',(req,res)=>{
+authrouter.get('/logout', (req, res) => {
     req.logout()
     res.redirect('/')
 })
