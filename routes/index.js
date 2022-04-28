de07632e83aac4aa654ee1da7cd1c1b3dfe1f59f
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-04-28 19:10:36 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
-const express= require('express');
+const express = require('express');
 const router = express.Router();
-const {ensureAuth , ensureGuest} = require('../middleware/auth')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-router.get('/',ensureGuest,(req,res)=>{
-    res.render('login',{
-        layouts:'login'
+router.get('/', ensureGuest, (req, res) => {
+    res.render('login', {
+        layouts: 'login'
     })
 })
 
-router.get('/dashboard',ensureAuth,(req,res)=>{
-    res.render("dashboard",{
-        layouts:'main'
+router.get('/dashboard', ensureAuth, (req, res) => {
+    res.render("dashboard", {
+        layouts: 'main'
     })
 })
 
-module.exports = {router}
+module.exports = { router }
