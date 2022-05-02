218b30e77a24e7e729f93b96d2d45af477e8d594
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-05-02 12:33:02 +0530

    select type pri or pub added

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -33,16 +33,16 @@ module.exports = {
           return ''
         }
       },
-    //   select: function (selected, options) {
-    //     return options
-    //       .fn(this)
-    //       .replace(
-    //         new RegExp(' value="' + selected + '"'),
-    //         '$& selected="selected"'
-    //       )
-    //       .replace(
-    //         new RegExp('>' + selected + '</option>'),
-    //         ' selected="selected"$&'
-    //       )
-    //   }
+      select: function (selected, options) {
+        return options
+          .fn(this)
+          .replace(
+            new RegExp(' value="' + selected + '"'),
+            '$& selected="selected"'
+          )
+          .replace(
+            new RegExp('>' + selected + '</option>'),
+            ' selected="selected"$&'
+          )
+      }
 }
