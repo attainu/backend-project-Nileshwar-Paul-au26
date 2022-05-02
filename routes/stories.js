89cc1c780484e9597da4bbd215cfbc4ea3c917dd
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-05-02 13:03:34 +0530

    put  /:id added

--- routes/stories.js
+++ routes/stories.js
@@ -72,4 +72,31 @@ blogrouter.get('/edit/:id', ensureAuth, async (req, res) => {
     }
   })
 
+// @desc    Update story
+// @route   PUT /stories/:id
+blogrouter.put('/:id', ensureAuth, async (req, res) => {
+    try {
+      let story = await Story.findById(req.params.id).lean()
+  
+      if (!story) {
+        return res.render('error/404')
+      }
+  
+      if (story.user != req.user.id) {
+        res.redirect('/stories')
+      } else {
+        story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+          new: true,
+          runValidators: true,
+        })
+  
+        res.redirect('/dashboard')
+      }
+    } catch (err) {
+      console.error(err)
+      return res.render('error/500')
+    }
+  })
+  
+
 module.exports = { blogrouter }
